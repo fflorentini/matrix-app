667992a6dd585f7e15e3706714e7e11a5e0da4a7
fflorentini <flavio.florentini.p@gmail.com>
2026-07-28 03:08:00 -0500

    Chore: Remove statistics api debug logging

--- go-api/services/statistics_client.go
+++ go-api/services/statistics_client.go
@@ -3,8 +3,6 @@ package services
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
 	"matrix-app/go-api/models"
 	"net/http"
 	"os"
@@ -42,24 +40,11 @@ func FetchStatistics(
 
 	defer resp.Body.Close()
 
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println("Statistics API Status:", resp.StatusCode)
-	fmt.Println("Statistics API Body:", string(bodyBytes))
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf(
-			"statistics api returned status %d",
-			resp.StatusCode,
-		)
-	}
-
 	var result models.StatisticsResponse
 
-	err = json.Unmarshal(bodyBytes, &result)
+	err = json.NewDecoder(resp.Body).
+		Decode(&result)
+
 	if err != nil {
 		return nil, err
 	}
